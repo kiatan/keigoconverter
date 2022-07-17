368bebd0e302108ea77cb43785bd9235791d70a2
Kia <divinekiat@gmail.com>
2022-07-17 15:57:31 +0900

    Updates

---
+++ precache-manifest.74b7a03bf3e3d8c2014396737ccf8e5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c7dcc9fd9676ffa3b66",
+    "url": "/keigoconverter/static/css/main.b72d8c8b.chunk.css"
+  },
+  {
+    "revision": "0c7dcc9fd9676ffa3b66",
+    "url": "/keigoconverter/static/js/main.0c7dcc9f.chunk.js"
+  },
+  {
+    "revision": "0838ac89c27b80834617",
+    "url": "/keigoconverter/static/js/1.0838ac89.chunk.js"
+  },
+  {
+    "revision": "874f5c95e3f8a3a8e386",
+    "url": "/keigoconverter/static/css/2.a000fcb0.chunk.css"
+  },
+  {
+    "revision": "874f5c95e3f8a3a8e386",
+    "url": "/keigoconverter/static/js/2.874f5c95.chunk.js"
+  },
+  {
+    "revision": "d6d3d86caed82d1a6a0d",
+    "url": "/keigoconverter/static/js/runtime~main.d6d3d86c.js"
+  },
+  {
+    "revision": "857914f4424802683cf829c06c370d60",
+    "url": "/keigoconverter/static/media/logo.857914f4.png"
+  },
+  {
+    "revision": "ba0fd23b3d068a5008685cbb2789b13a",
+    "url": "/keigoconverter/index.html"
+  }
+];
